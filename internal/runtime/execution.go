d368fb7a8d9935f618cfa2ba54a9e9101dd91f28
Alberto Elorza <albertoelorza2002@gmail.com>
2022-06-24 12:02:23 +0200

    instruction table

--- internal/runtime/execution.go
+++ internal/runtime/execution.go
@@ -189,6 +189,27 @@ func (proc *Process) GetWorkingObject() Object {
 	return proc.workingObject
 }
 
+func (proc *Process) a(x Instruction) Object {
+	if x.sz < 1 {
+		return proc.functionstack.Pop()
+	}
+	return x.operands[0]
+}
+
+func (proc *Process) b(x Instruction) Object {
+	if x.sz < 2 {
+		return proc.functionstack.Pop()
+	}
+	return x.operands[1]
+}
+
+func boolNum(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
+
 /*
 Run zone
 */
@@ -205,318 +226,205 @@ func (proc *Process) run() {
 	for proc.pc < len(proc.symbol.Source) {
 		ins := proc.symbol.Source[proc.pc]
 		proc.pc++
-		opNum := opNumTable[ins.Code]
-		if opNum != 0 {
-			sz := ins.sz
-			switch opNum - sz {
-			case 1:
-				ins.operands[sz] = proc.functionstack.Pop()
-			case 2:
-				ins.operands[0] = proc.functionstack.Pop()
-				ins.operands[1] = proc.functionstack.Pop()
+		code := ins.Code
+		switch {
+		case code < 64:
+			switch code {
+			case NOP:
+			//ARITHMETIC
+			case ADD:
+				proc.functionstack.Push(proc.a(ins).(int) + proc.b(ins).(int))
+			case SUB:
+				proc.functionstack.Push(proc.a(ins).(int) - proc.b(ins).(int))
+			case MUL:
+				proc.functionstack.Push(proc.a(ins).(int) * proc.b(ins).(int))
+			case DIV:
+				proc.functionstack.Push(proc.a(ins).(int) / proc.b(ins).(int))
+			case MOD:
+				proc.functionstack.Push(proc.a(ins).(int) % proc.b(ins).(int))
+			case ADDF:
+				proc.functionstack.Push(proc.a(ins).(float64) + proc.b(ins).(float64))
+			case SUBF:
+				proc.functionstack.Push(proc.a(ins).(float64) - proc.b(ins).(float64))
+			case MULF:
+				proc.functionstack.Push(proc.a(ins).(float64) * proc.b(ins).(float64))
+			case DIVF:
+				proc.functionstack.Push(proc.a(ins).(float64) / proc.b(ins).(float64))
+			//CONVERSION
+			case ITD:
+				proc.functionstack.Push(float64(proc.a(ins).(int)))
+			case DTI:
+				proc.functionstack.Push(int(proc.a(ins).(float64)))
+			case IRE:
+				proc.functionstack.Push(strconv.Itoa(proc.a(ins).(int)))
+			case DRE:
+				proc.functionstack.Push(fmt.Sprintf("%g", proc.a(ins).(float64)))
+			case IPA:
+				i, e := strconv.Atoi(proc.a(ins).(string))
+				if e != nil {
+					panic(e)
+				}
+				proc.functionstack.Push(i)
+			case DPA:
+				f, e := strconv.ParseFloat(proc.a(ins).(string), 64)
+				if e != nil {
+					panic(e)
+				}
+				proc.functionstack.Push(f)
+			//COMPARISON
+			case EQI:
+				proc.functionstack.Push(boolNum(proc.a(ins).(int) == proc.b(ins).(int)))
+			case EQD:
+				proc.functionstack.Push(boolNum(proc.a(ins).(float64) == proc.b(ins).(float64)))
+			case EQS:
+				proc.functionstack.Push(boolNum(proc.a(ins).(string) == proc.b(ins).(string)))
+			case ILE:
+				proc.functionstack.Push(boolNum(proc.a(ins).(int) < proc.b(ins).(int)))
+			case DLE:
+				proc.functionstack.Push(boolNum(proc.a(ins).(float64) < proc.b(ins).(float64)))
+			case IGR:
+				proc.functionstack.Push(boolNum(proc.a(ins).(int) > proc.b(ins).(int)))
+			case DGR:
+				proc.functionstack.Push(boolNum(proc.a(ins).(float64) > proc.b(ins).(float64)))
+			case ILQ:
+				proc.functionstack.Push(boolNum(proc.a(ins).(int) <= proc.b(ins).(int)))
+			case DLQ:
+				proc.functionstack.Push(boolNum(proc.a(ins).(float64) <= proc.b(ins).(float64)))
+			case IGQ:
+				proc.functionstack.Push(boolNum(proc.a(ins).(int) >= proc.b(ins).(int)))
+			case DGQ:
+				proc.functionstack.Push(boolNum(proc.a(ins).(float64) >= proc.b(ins).(float64)))
+			//LOGIC
+			case NOT:
+				proc.functionstack.Push(^proc.a(ins).(int))
+			case AND:
+				proc.functionstack.Push(proc.a(ins).(int) & proc.b(ins).(int))
+			case OR:
+				proc.functionstack.Push(proc.a(ins).(int) | proc.b(ins).(int))
+			case XOR:
+				proc.functionstack.Push(proc.a(ins).(int) ^ proc.b(ins).(int))
+			//STRINGS
+			case CCS:
+				proc.functionstack.Push(proc.a(ins).(string) + proc.b(ins).(string))
+			case CAI:
+				proc.functionstack.Push(int(proc.a(ins).(string)[proc.b(ins).(int)]))
+			}
+		case code < 128:
+			switch code {
+			//MEMORY
+			case LEI:
+				proc.functionstack.Push(proc.env.GetEnvironment(proc.a(ins).(int)))
+			case SEI:
+				proc.env.SetEnvironment(proc.a(ins).(int), proc.b(ins))
+			case LLI:
+				proc.functionstack.Push(proc.locals.GetLocal(proc.a(ins).(int)))
+			case SLI:
+				proc.locals.SetLocal(proc.a(ins).(int), proc.b(ins))
+			//STACK
+			case PSH:
+				proc.functionstack.Push(proc.a(ins))
+			case POP:
+				proc.functionstack.Pop()
+			case CLR:
+				proc.functionstack.Clear()
+			case DUP:
+				x := proc.a(ins)
+				proc.functionstack.Push(x)
+				proc.functionstack.Push(x)
+			case SWT:
+				x, y := proc.a(ins), proc.b(ins)
+				proc.functionstack.Push(x)
+				proc.functionstack.Push(y)
+			//CONTROL
+			case CLL:
+				proc.CallFragment(proc.a(ins).(string))
+			case JMP:
+				proc.JumpToFragment(proc.a(ins).(string))
+			case RET:
+				proc.ReturnLastPoint()
+			case SKT:
+				if proc.a(ins).(int) != 0 {
+					proc.pc++
+				}
+			case SKF:
+				if proc.a(ins).(int) == 0 {
+					proc.pc++
+				}
+			case MVR:
+				proc.pc += proc.a(ins).(int)
+			case MVT:
+				if proc.b(ins).(int) != 0 {
+					proc.pc += proc.a(ins).(int)
+				}
+			case MVF:
+				if proc.b(ins).(int) == 0 {
+					proc.pc += proc.a(ins).(int)
+				}
+			}
+		case code < 192:
+			switch code {
+			//MAPS AND VECTORS
+			case KVC:
+				proc.SetWorkingObject(make(MapT))
+			case PRP:
+				proc.functionstack.Push((proc.GetWorkingObject().(MapT))[proc.a(ins).(string)])
+			case ATT:
+				(proc.GetWorkingObject().(MapT))[proc.a(ins).(string)] = proc.b(ins)
+			case EXK:
+				_, exists := (proc.GetWorkingObject().(MapT))[proc.a(ins).(string)]
+				proc.functionstack.Push(boolNum(exists))
+			case VEC:
+				vec := make([]Object, 0)
+				var vecref VecT = &vec
+				proc.SetWorkingObject(vecref)
+			case ACC:
+				proc.functionstack.Push((*(proc.GetWorkingObject().(VecT)))[proc.a(ins).(int)])
+			case APP:
+				vec := proc.GetWorkingObject().(VecT)
+				*vec = append(*vec, proc.b(ins))
+			case SVI:
+				vec := *(proc.GetWorkingObject().(VecT))
+				vec[proc.a(ins).(int)] = proc.b(ins)
+			case DMI:
+				m := proc.GetWorkingObject().(MapT)
+				delete(m, proc.a(ins).(string))
+			case CSE:
+				vec := make([]Object, 0)
+				var vecref VecT = &vec
+				sz := proc.a(ins).(int)
+				if sz < 0 {
+					panic("Trying to collapse negative number of elements")
+				}
+				for i := 0; i < sz; i++ {
+					res := proc.functionstack.Pop()
+					*vecref = append(*vecref, res)
+				}
+				proc.SetWorkingObject(vecref)
+			case WTP:
+				proc.functionstack.Push(proc.GetWorkingObject())
+			case PTW:
+				proc.SetWorkingObject(proc.a(ins))
+			//SIZE
+			case SOS:
+				proc.functionstack.Push(len(proc.a(ins).(string)))
+			case SOV:
+				proc.functionstack.Push(len(*proc.a(ins).(VecT)))
+			case SOM:
+				proc.functionstack.Push(len(proc.a(ins).(MapT)))
+			}
+		default:
+			switch code {
+			//STATE
+			case SWR:
+				proc.state = proc.a(ins)
+			case SRE:
+				proc.functionstack.Push(proc.state)
+			//Interaction
+			case INV:
+				proc.Invoke(proc.a(ins).(string))
+			case IFD:
+				proc.DirectInvoke(proc.a(ins).(EmbeddedFunction))
 			}
 		}
-		//fn.Action(proc, ins.operands)
-		switch ins.Code {
-		case NOP:
-		//ARITHMETIC
-		case ADD:
-			proc.functionstack.Push(ins.operands[0].(int) + ins.operands[1].(int))
-
-		case SUB:
-			proc.functionstack.Push(ins.operands[0].(int) - ins.operands[1].(int))
-
-		case MUL:
-			proc.functionstack.Push(ins.operands[0].(int) * ins.operands[1].(int))
-
-		case DIV:
-			proc.functionstack.Push(ins.operands[0].(int) / ins.operands[1].(int))
-
-		case MOD:
-			proc.functionstack.Push(ins.operands[0].(int) % ins.operands[1].(int))
-
-		case ADDF:
-			proc.functionstack.Push(ins.operands[0].(float64) + ins.operands[1].(float64))
-
-		case SUBF:
-			proc.functionstack.Push(ins.operands[0].(float64) - ins.operands[1].(float64))
-
-		case MULF:
-			proc.functionstack.Push(ins.operands[0].(float64) * ins.operands[1].(float64))
-
-		case DIVF:
-			proc.functionstack.Push(ins.operands[0].(float64) / ins.operands[1].(float64))
-
-		//CONVERSION
-		case ITD:
-			proc.functionstack.Push(float64(ins.operands[0].(int)))
-
-		case DTI:
-			proc.functionstack.Push(int(ins.operands[0].(float64)))
-
-		case IRE:
-			proc.functionstack.Push(strconv.Itoa(ins.operands[0].(int)))
-
-		case DRE:
-			proc.functionstack.Push(fmt.Sprintf("%g", ins.operands[0].(float64)))
-
-		case IPA:
-			i, e := strconv.Atoi(ins.operands[0].(string))
-			if e != nil {
-				panic(e)
-			}
-			proc.functionstack.Push(i)
-
-		case DPA:
-			f, e := strconv.ParseFloat(ins.operands[0].(string), 64)
-			if e != nil {
-				panic(e)
-			}
-			proc.functionstack.Push(f)
-
-		//COMPARISON
-		case EQI:
-			if ins.operands[0].(int) == ins.operands[1].(int) {
-				proc.functionstack.Push(1)
-			} else {
-				proc.functionstack.Push(0)
-			}
-
-		case EQD:
-			if ins.operands[0].(float64) == ins.operands[1].(float64) {
-				proc.functionstack.Push(1)
-			} else {
-				proc.functionstack.Push(0)
-			}
-
-		case EQS:
-			if ins.operands[0].(string) == ins.operands[1].(string) {
-				proc.functionstack.Push(1)
-			} else {
-				proc.functionstack.Push(0)
-			}
-
-		case ILE:
-			if ins.operands[0].(int) < ins.operands[1].(int) {
-				proc.functionstack.Push(1)
-			} else {
-				proc.functionstack.Push(0)
-			}
-
-		case DLE:
-			if ins.operands[0].(float64) < ins.operands[1].(float64) {
-				proc.functionstack.Push(1)
-			} else {
-				proc.functionstack.Push(0)
-			}
-
-		case IGR:
-			if ins.operands[0].(int) > ins.operands[1].(int) {
-				proc.functionstack.Push(1)
-			} else {
-				proc.functionstack.Push(0)
-			}
-
-		case DGR:
-			if ins.operands[0].(float64) > ins.operands[1].(float64) {
-				proc.functionstack.Push(1)
-			} else {
-				proc.functionstack.Push(0)
-			}
-
-		case ILQ:
-			if ins.operands[0].(int) <= ins.operands[1].(int) {
-				proc.functionstack.Push(1)
-			} else {
-				proc.functionstack.Push(0)
-			}
-
-		case DLQ:
-			if ins.operands[0].(float64) <= ins.operands[1].(float64) {
-				proc.functionstack.Push(1)
-			} else {
-				proc.functionstack.Push(0)
-			}
-
-		case IGQ:
-			if ins.operands[0].(int) >= ins.operands[1].(int) {
-				proc.functionstack.Push(1)
-			} else {
-				proc.functionstack.Push(0)
-			}
-
-		case DGQ:
-			if ins.operands[0].(float64) >= ins.operands[1].(float64) {
-				proc.functionstack.Push(1)
-			} else {
-				proc.functionstack.Push(0)
-			}
-
-		//LOGIC
-		case NOT:
-			proc.functionstack.Push(^ins.operands[0].(int))
-
-		case AND:
-			proc.functionstack.Push(ins.operands[0].(int) & ins.operands[1].(int))
-
-		case OR:
-			proc.functionstack.Push(ins.operands[0].(int) | ins.operands[1].(int))
-
-		case XOR:
-			proc.functionstack.Push(ins.operands[0].(int) ^ ins.operands[1].(int))
-
-		//STRINGS
-		case CCS:
-			proc.functionstack.Push(ins.operands[0].(string) + ins.operands[1].(string))
-
-		case CAI:
-			proc.functionstack.Push(int(ins.operands[0].(string)[ins.operands[1].(int)]))
-
-		//MEMORY
-		case LEI:
-			proc.functionstack.Push(proc.env.GetEnvironment(ins.operands[0].(int)))
-
-		case SEI:
-			proc.env.SetEnvironment(ins.operands[0].(int), ins.operands[1])
-
-		case LLI:
-			proc.functionstack.Push(proc.locals.GetLocal(ins.operands[0].(int)))
-
-		case SLI:
-			proc.locals.SetLocal(ins.operands[0].(int), ins.operands[1])
-
-		//STACK
-		case PSH:
-			proc.functionstack.Push(ins.operands[0])
-
-		case POP:
-			//Just consumes the value
-
-		case CLR:
-			proc.functionstack.Clear()
-
-		case DUP:
-			proc.functionstack.Push(ins.operands[0])
-			proc.functionstack.Push(ins.operands[0])
-
-		case SWT:
-			proc.functionstack.Push(ins.operands[0])
-			proc.functionstack.Push(ins.operands[1])
-
-		//CONTROL
-		case CLL:
-			proc.CallFragment(ins.operands[0].(string))
-
-		case JMP:
-			proc.JumpToFragment(ins.operands[0].(string))
-
-		case RET:
-			proc.ReturnLastPoint()
-
-		case SKT:
-			if ins.operands[0].(int) != 0 {
-				proc.pc++
-			}
-
-		case SKF:
-			if ins.operands[0].(int) == 0 {
-				proc.pc++
-			}
-
-		case MVR:
-			proc.pc += ins.operands[0].(int)
-
-		case MVT:
-			if ins.operands[1].(int) != 0 {
-				proc.pc += ins.operands[0].(int)
-			}
-
-		case MVF:
-			if ins.operands[1].(int) == 0 {
-				proc.pc += ins.operands[0].(int)
-			}
-
-		//MAPS AND VECTORS
-		case KVC:
-			proc.SetWorkingObject(make(MapT))
-
-		case PRP:
-			proc.functionstack.Push((proc.GetWorkingObject().(MapT))[ins.operands[0].(string)])
-
-		case ATT:
-			(proc.GetWorkingObject().(MapT))[ins.operands[0].(string)] = ins.operands[1]
-
-		case EXK:
-			if _, exists := (proc.GetWorkingObject().(MapT))[ins.operands[0].(string)]; exists {
-				proc.functionstack.Push(1)
-			} else {
-				proc.functionstack.Push(0)
-			}
-
-		case VEC:
-			vec := make([]Object, 0)
-			var vecref VecT = &vec
-			proc.SetWorkingObject(vecref)
-
-		case ACC:
-			proc.functionstack.Push((*(proc.GetWorkingObject().(VecT)))[ins.operands[0].(int)])
-
-		case APP:
-			vec := proc.GetWorkingObject().(VecT)
-			*vec = append(*vec, ins.operands[1])
-
-		case SVI:
-			vec := *(proc.GetWorkingObject().(VecT))
-			vec[ins.operands[0].(int)] = ins.operands[1]
-
-		case DMI:
-			m := proc.GetWorkingObject().(MapT)
-			delete(m, ins.operands[0].(string))
-
-		case CSE:
-			vec := make([]Object, 0)
-			var vecref VecT = &vec
-			sz := ins.operands[0].(int)
-			if sz < 0 {
-				panic("Trying to collapse negative number of elements")
-			}
-			for i := 0; i < sz; i++ {
-				res := proc.functionstack.Pop()
-				*vecref = append(*vecref, res)
-			}
-			proc.SetWorkingObject(vecref)
-
-		case WTP:
-			proc.functionstack.Push(proc.GetWorkingObject())
-
-		case PTW:
-			proc.SetWorkingObject(ins.operands[0])
-
-		//SIZE
-
-		case SOS:
-			proc.functionstack.Push(len(ins.operands[0].(string)))
-
-		case SOV:
-			proc.functionstack.Push(len(*ins.operands[0].(VecT)))
-
-		case SOM:
-			proc.functionstack.Push(len(ins.operands[0].(MapT)))
-
-		//STATE
-		case SWR:
-			proc.state = ins.operands[0]
-
-		case SRE:
-			proc.functionstack.Push(proc.state)
-
-		//Interaction
-		case INV:
-			proc.Invoke(ins.operands[0].(string))
-
-		case IFD:
-			proc.DirectInvoke(ins.operands[0].(EmbeddedFunction))
-		}
-
 	}
 }
