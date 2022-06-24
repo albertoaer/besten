518e6bb405d1ae8534f62b30793dd96c736384be
Alberto Elorza <albertoelorza2002@gmail.com>
2022-06-24 12:02:20 +0200

    symdump tool

---
+++ cmd/symdump/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/Besten/internal/modules"
+	"github.com/Besten/internal/runtime"
+)
+
+func itable(path string, begin int) map[int]string {
+	b, e := ioutil.ReadFile(path)
+	if e != nil {
+		panic(e)
+	}
+	result := make(map[int]string)
+	lines := strings.Split(string(b), "\n")
+	for i := begin; i < len(lines); i++ {
+		n := strings.Split(lines[i], "/")
+		if len(n) > 0 {
+			target := strings.TrimSpace(n[0])
+			if target == ")" {
+				break
+			}
+			if len(target) > 0 && target[0] != '/' {
+				parts := strings.Split(target, "=")
+				ids := strings.Split(parts[0], " ")
+				icode, e := strconv.Atoi(strings.TrimSpace(parts[1]))
+				if e != nil {
+					panic(e)
+				}
+				result[icode] = ids[0]
+			}
+		}
+	}
+	return result
+}
+
+func dumpInto(s runtime.Symbol, table map[int]string, dest io.Writer) {
+	fmt.Fprintln(dest, "Dumped from: ", s.Name)
+	for i, v := range s.Source {
+		fmt.Fprintf(dest, "\t%4d %s", i, table[int(v.Code)])
+		for _, v := range v.Inspect() {
+			fmt.Fprint(dest, " ", v)
+		}
+		fmt.Fprintln(dest)
+	}
+}
+
+func main() {
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Println("Error while dumping: \n\t", e)
+		}
+	}()
+	table := itable("./internal/runtime/instructionset.go", 10)
+	var file string
+	var name string
+	flag.StringVar(&file, "file", "", "File to be compiled")
+	flag.StringVar(&name, "name", "", "Prefix to compare the symbol compilation name")
+	flag.Parse()
+	if len(file) == 0 {
+		panic("No file provided")
+	}
+	symbols, _, err := modules.New().MainFile(file)
+	if err != nil {
+		panic(err)
+	}
+	for k, s := range symbols {
+		if strings.HasPrefix(k, name) {
+			dumpInto(s, table, os.Stdout)
+		}
+	}
+	fmt.Println("Dumping done!")
+}
